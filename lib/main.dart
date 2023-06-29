80764b4262b66b4bc9036210f6de5749e9c4def5
Dwaipayan Biswas <biswasdwai007@gmail.com>
2023-06-29 13:15:43 +0530

    Adding Database

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Yoga App',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Finish(),
+      home:  Home(),
 
 
     );
